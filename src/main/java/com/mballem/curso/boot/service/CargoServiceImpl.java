a698dd4ddec97ae5292665fc935ebd170dab75e9
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2019-08-02 21:12:56 -0300

    Excluindo Cargos

--- src/main/java/com/mballem/curso/boot/service/CargoServiceImpl.java
+++ src/main/java/com/mballem/curso/boot/service/CargoServiceImpl.java
@@ -40,4 +40,12 @@ public class CargoServiceImpl implements CargoService {
 	public List<Cargo> buscarTodos() {
 		return dao.findAll();
 	}
+
+	@Override
+	public boolean cargoTemFuncionarios(Long id) {
+		if (buscarPorId(id).getFuncionarios().isEmpty()) {
+			return false;
+		}
+		return true;
+	}
 }
--- src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
@@ -62,6 +62,17 @@ public class CargoController {
 		return "redirect:/cargos/cadastrar";
 	}
 	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		if (cargoService.cargoTemFuncionarios(id)) {
+			attr.addFlashAttribute("fail", "Cargo não excluido. Tem funcionário(s) vinculado(s).");
+		} else {
+			cargoService.excluir(id);
+			attr.addFlashAttribute("success", "Cargo excluido com sucesso.");
+		}
+		return "redirect:/cargos/listar";
+	}
+	
 	@ModelAttribute("departamentos")
 	public List<Departamento> listaDepartamentos() {
 		return departamentoService.buscarTodos();
