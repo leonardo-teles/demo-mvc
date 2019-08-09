f33da3befcc379ea9755028c0139fa0ff8ecd50c
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2019-08-09 18:15:57 -0300

    Buscar Funcionário por Nome

--- src/main/java/com/mballem/curso/boot/dao/FuncionarioDaoImpl.java
+++ src/main/java/com/mballem/curso/boot/dao/FuncionarioDaoImpl.java
@@ -1,10 +1,17 @@
 package com.mballem.curso.boot.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.mballem.curso.boot.domain.Funcionario;
 
 @Repository
 public class FuncionarioDaoImpl extends AbstractDao<Funcionario, Long> implements FuncionarioDao {
+
+	@Override
+	public List<Funcionario> findByNome(String nome) {
+		return createQuery("select f from Funcionario f where f.nome like concat('%',?1,'%')", nome);
+	}
 
 }
